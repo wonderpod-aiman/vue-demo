bc9bacdb5e5e0220071a732bab6bb072a9e07a35
Aiman Shamsiemon <maimansham@gmail.com>
2022-11-23 11:22:44 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import MainPokemon from './components/MainPokemon.vue';
+</script>
+
+<template>
+  <header>
+    <h3>Display Pokemon data</h3>
+  </header>
+
+  <MainPokemon>
+  </MainPokemon>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/MainPokemon.vue
@@ -0,0 +1,39 @@
+<script PokeAPI>
+    export default{
+        // make a request to PokeAPI
+        data(){
+            return {
+                pokemonData: null,
+            };
+        },
+        methods: {
+            async fetchData() {
+                const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10000')
+                    .then((response) => {
+                        if (response.status != 200){
+                            alert("Something Went Wrong!");
+                            return;
+                        }
+
+                        response.json().then(data => {this.pokemonData = data.results})
+                    }
+                );
+
+            }
+        },
+
+
+        mounted(){
+            this.fetchData();
+        },
+    }
+        
+</script>
+
+<template>
+    <div>
+        <li v-for="pokemon in pokemonData">
+            {{ pokemon.name }}
+        </li>
+    </div>
+</template>
